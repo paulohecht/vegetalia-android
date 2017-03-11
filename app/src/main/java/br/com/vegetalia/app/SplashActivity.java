22443c577158b9d316e78dd367cd177c1bf57cce
Paulo Hecht <thehecht@gmail.com>
2017-03-11 13:55:32 -0300

    firebase integration and anonymous login

--- app/src/main/java/br/com/vegetalia/app/SplashActivity.java
+++ app/src/main/java/br/com/vegetalia/app/SplashActivity.java
@@ -1,8 +1,15 @@
 package br.com.vegetalia.app;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,8 +18,20 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
-        startActivity(new Intent(this, MainActivity.class));
+        FirebaseAuth.getInstance()
+                .signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SplashActivity.this, R.string.splash_authentication_error, Toast.LENGTH_SHORT).show();
+                            return;
+                            //TODO: REDIRECT TO FAIL SCREEN...
+                        }
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        finish();
+                    }
+                });
 
     }
 }
